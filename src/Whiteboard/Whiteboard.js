426a688633c5403c452a7b9fb346089f9deaa147
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-06-19 22:22:02 +0530

    add roughjs

--- src/Whiteboard/Whiteboard.js
+++ src/Whiteboard/Whiteboard.js
@@ -1,9 +1,53 @@
+import { useRef, useLayoutEffect, useState } from "react";
 import Menu from "./Menu";
+import rough from "roughjs/bundled/rough.esm";
+import { useSelector } from "react-redux";
+import { actions, toolTypes } from "../constants";
+import { createElement } from "./utils";
+import { v4 as uuid } from "uuid";
 
 const Whiteboard = () => {
+  const canvasRef = useRef();
+  const toolType = useSelector((state) => state.whiteboard.tool);
+  const [action, setAction] = useState(null);
+
+  useLayoutEffect(() => {
+    const canvas = canvasRef.current;
+    const rc = rough.canvas(canvas);
+    rc.rectangle(10, 10, 200, 200);
+    rc.rectangle(20, 20, 300, 300);
+    rc.line(80, 120, 300, 100);
+    rc.line(0, 0, 100, 100);
+  }, []);
+
+  const handleMouseDown = (event) => {
+    const { clientX, clientY } = event;
+
+    if (toolType === toolTypes.RECTANGLE) {
+      setAction(actions.DRAWING);
+    }
+
+    const element = createElement({
+      x1: clientX,
+      y1: clientY,
+      x2: clientX,
+      y2: clientY,
+      toolType,
+      id: uuid(),
+    });
+
+    console.log(element);
+  };
+
   return (
     <>
       <Menu />
+      <canvas
+        onMouseDown={handleMouseDown}
+        ref={canvasRef}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      />
     </>
   );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,2 @@
 export { toolTypes } from "./tooltype";
+export { actions } from "./action";
